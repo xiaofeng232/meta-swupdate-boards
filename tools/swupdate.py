b542eee8e90ac3290747ff703f5d3d0289c6e2e6
xiaofeng.lin <xiaofeng.lin@wingd.com>
2025-12-04 07:50:08 +0100

    Add tool and ci file

---
+++ tools/swupdate.py
@@ -0,0 +1,54 @@
+import asyncio
+import json
+import os
+import requests
+import websockets
+import sys
+
+
+class SWUpdater:
+    """ Python helper class for SWUpdate """
+
+    url_upload = 'http://{}:8080/upload'
+    url_status = 'ws://{}:8080/ws/'
+
+    def __init__(self, path_image, host_name):
+        self.__image = path_image
+        self.__host_name = host_name
+
+    async def __wait_update_finished(self, timeout=300):
+        async def get_finish_messages():
+            async with websockets.connect(self.url_status.format(self.__host_name)) as websocket:
+                while True:
+                    message = await websocket.recv()
+                    data = json.loads(message)
+
+                    if data["type"] != "message":
+                        continue
+
+                    print(data["text"])
+                    if data["text"] == "SWUPDATE successful !":
+                        return
+
+        await asyncio.wait_for(get_finish_messages(), timeout=timeout)
+
+    def update(self, timeout=300):
+        print("Start uploading image...")
+        response = requests.post(self.url_upload.format(self.__host_name), files={'file': open(self.__image, 'rb')})
+
+        if response.status_code != 200:
+            raise Exception("Cannot upload software image: {}".format(response.status_code))
+
+        print("Software image uploaded successfully. Wait for installation to be finished...\n")
+
+        asyncio.get_event_loop().run_until_complete(self.__wait_update_finished(timeout=timeout))
+
+
+if __name__ == "__main__":
+    sys.path.append(os.getcwd())
+
+    if len(sys.argv) != 3:
+        print("Usage: swupdate.py <path to image> <hostname>")
+        exit(0)
+
+    SWUpdater(sys.argv[1], sys.argv[2]).update()
